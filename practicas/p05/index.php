3c268636cfc66b6278203f342e3a9c766f1aa714
AlanTrujilloRoldan <alantrujillo234@gmail.com>
2024-09-03 21:14:09 -0600

    Variables modificadas y analizadas

--- practicas/p05/index.php
+++ practicas/p05/index.php
@@ -14,7 +14,33 @@
     echo "La variable \$var7 es válida porque respeta no empezar con un número" . '<br />';
     echo "La variable \$_element1 es válida porque respeta no empezar con un número" . '<br />';
     echo "La variable \$house*5 NO es válida porque contiene un carácter no permitido (*)" . '<br />';    
+
+
+    $a = "ManejadorSQL";
+    $b = 'MySQL';
+    $c = &$a;
+    echo '<br />';
+    echo $a. '<br />';
+    echo $b. '<br />';
+    echo $c. '<br />';
+
+    echo '<br />';
+    $a = "PHP server";
+    $b = &$a;
+    echo $a. '<br />';
+    echo $b. '<br />';
+    echo $c. '<br />';
     ?>
+    <p>
+        En el primer bloque, $c refencia a la variable $a, mientras que $b es una variable con un valor distinto 'MySQL'.
+        Esto hace que cualquier cambio en $a se refleje en $c, pero no afecta a $b. 
+        En el segundo bloque, tanto $b como $c son referencias a $a, lo que significa que cualquier 
+        cambio en $a se refleja automáticamente en ambas, haciendo que las tres variables
+        compartan el mismo valor "PHP server".
+        <br> 
+        La principal diferencia es que en el segundo bloque,$b deja de ser una variable independiente
+        y se convierte en una referencia a $a, igualando su valor con el de $a y $c.
+    </p>
 </body>
 
 </html>
