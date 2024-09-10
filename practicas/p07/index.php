f9d0f2c0a3a52721d01141d7ae588c5b2a0e99c1
AlanTrujilloRoldan <alantrujillo234@gmail.com>
2024-09-10 13:43:18 -0600

    Formulario para verificar multiplos de 5 y 7 creado

--- practicas/p07/index.php
+++ practicas/p07/index.php
@@ -3,11 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Práctica 4</title>
+    <title>Práctica 7</title>
 </head>
 <body>
     <h2>Ejercicio 1</h2>
     <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
+    <form action="index.php" method="get">
+        <input type="number" name="numero">
+        <input type="submit" value="Validar">
+    </form>
     <?php
         if(isset($_GET['numero']))
         {
