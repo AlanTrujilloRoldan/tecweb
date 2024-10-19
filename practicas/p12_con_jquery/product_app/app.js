85d2a3a6b2ccad4e92f28f85b0f37d94a1b07e5c
AlanTrujilloRoldan <alantrujillo234@gmail.com>
2024-10-19 00:41:33 -0600

    Actualización de la lista cuando se realiza una busqueda corregida

--- practicas/p12_con_jquery/product_app/app.js
+++ practicas/p12_con_jquery/product_app/app.js
@@ -23,11 +23,10 @@ $(document).ready(function () {
   $('#product-result').hide();
   fetchProducts();
 
-  //Busqueda de los productos
-  //Pendiente (Mostrar en la lista normal??)
+  // Búsqueda de los productos
   $('#search').keyup(function (e) {
-    if ($('#search').val()) {
-      let search = $('#search').val();
+    let search = $('#search').val();
+    if (search) {
       $.ajax({
         url: './backend/product-search.php',
         type: 'GET',
@@ -36,18 +35,43 @@ $(document).ready(function () {
           let products = JSON.parse(response);
           let template = '';
 
-          products.forEach(product => {
-            template += `<li>
-           ${product.nombre}
-          </li>`;
-          });
+          // Comprobar si hay productos
+          if (products.length > 0) {
+            products.forEach(product => {
+              template += `
+                          <tr productId="${product.id}">
+                              <td>${product.id}</td>
+                              <td>${product.nombre}</td>
+                              <td>
+                                  <ul>
+                                      <li>Precio: ${product.precio}</li>
+                                      <li>Unidades: ${product.unidades}</li>
+                                      <li>Modelo: ${product.modelo}</li>
+                                      <li>Marca: ${product.marca}</li>
+                                      <li>Detalles: ${product.detalles}</li>
+                                  </ul>
+                              </td>
+                              <td>
+                                  <button class="product-delete btn btn-danger"> 
+                                      Delete 
+                                  </button>
+                              </td>
+                          </tr>`;
+            });
+          } else {
+            template = '<tr><td colspan="4">No se encontraron productos.</td></tr>';
+          }
 
-          $('#container').html(template);
+          $('#products').html(template); // Actualizar la tabla con los resultados de búsqueda
           $('#product-result').show();
         }
-      })
+      });
+    } else {
+      fetchProducts(); // Volver a cargar todos los productos si no hay búsqueda
+      $('#product-result').hide();
     }
-  })
+  });
+
 
   //Pendiente
   //Mandar un JSON o un Archivo a Product-add
@@ -81,7 +105,6 @@ $(document).ready(function () {
       type: 'GET',
       success: function (response) {
         let products = JSON.parse(response);
-        console.log(products);
         let template = '';
         products.forEach(product => {
           template += `
