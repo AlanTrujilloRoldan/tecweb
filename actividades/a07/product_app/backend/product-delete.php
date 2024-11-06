3c1ccdcfbf09b151805adc5aa96fae9764cf9d8c
AlanTR <alantrujillo234@gmail.com>
2024-11-06 01:11:09 -0600

    Modificación en product-delete para POO realizada

---
+++ actividades/a07/product_app/backend/product-delete.php
@@ -0,0 +1,43 @@
+<?php
+/*
+    include_once __DIR__.'/database.php';
+
+    // SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
+    $data = array(
+        'status'  => 'error',
+        'message' => 'La consulta falló'
+    );
+    // SE VERIFICA HABER RECIBIDO EL ID
+    if( isset($_GET['id']) ) {
+        $id = $_GET['id'];
+        // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
+        $sql = "UPDATE productos SET eliminado=1 WHERE id = {$id}";
+        if ( $conexion->query($sql) ) {
+            $data['status'] =  "Success";
+            $data['message'] =  "Producto eliminado";
+		} else {
+            $data['message'] = "ERROR: No se ejecuto $sql. " . mysqli_error($conexion);
+        }
+		$conexion->close();
+    } 
+    
+    // SE HACE LA CONVERSIÓN DE ARRAY A JSON
+    echo json_encode($data, JSON_PRETTY_PRINT);
+*/
+namespace Backend;
+
+require_once __DIR__ . '/Products.php';
+
+use Products\Products;
+
+// Obtener el ID del producto desde la solicitud GET
+$id = $_GET['id'];
+
+// Crear una instancia de la clase Products
+$productApp = new Products();
+
+// Llamar al método delete para eliminar el producto
+$productApp->delete($id);
+
+// Devolver la respuesta en formato JSON
+echo $productApp->getData();
