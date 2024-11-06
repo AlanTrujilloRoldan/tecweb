86c8c824d2782c7b778c28fe5bd77cfb52525989
AlanTR <alantrujillo234@gmail.com>
2024-11-06 01:13:24 -0600

    Modificación y Creacion de product-single y productByName respectivamente realizada

---
+++ actividades/a07/product_app/backend/product-singleByName.php
@@ -0,0 +1,18 @@
+<?php
+namespace Backend;
+
+require_once __DIR__ . '/Products.php';
+
+use Products\Products;
+
+// Obtener el nombre del producto desde la solicitud GET
+$name = $_GET['name'];
+
+// Crear una instancia de la clase Products
+$productApp = new Products();
+
+// Obtener el producto por nombre
+$productApp->singleByName($name);
+
+// Devolver la respuesta en formato JSON
+echo $productApp->getData();
